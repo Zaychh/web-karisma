578687c87ffaf0d9c4eb57779ed69f9bccafcee3
Ilysh Zaychik <kyoraajaa@gmail.com>
2025-06-26 11:40:46 +0700

    uhhh update dikit

--- server/src/database.ts
+++ server/src/database.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db = mysql.createPool({
-    connectionLimit: 10,
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER ||'root',
     password: process.env.DB_PASSWORD || '',
