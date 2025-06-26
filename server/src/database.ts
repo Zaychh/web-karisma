b189ebd456ed93f9650b52c047355ca784ae11e9
Ilysh Zaychik <kyoraajaa@gmail.com>
2025-06-26 10:53:01 +0700

    update for railway

--- server/src/database.ts
+++ server/src/database.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export const db = mysql.createPool({
+    connectionLimit: 10,
     host: process.env.DB_HOST || 'localhost',
     user: process.env.DB_USER ||'root',
     password: process.env.DB_PASSWORD || '',
